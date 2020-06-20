3b86f707aa888413fa0386ff435143c987ed94a3
Prateek Sood <39465859+prateeksood@users.noreply.github.com>
2020-06-20 19:39:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,44 +14,51 @@ const order=(page)=>{
     try{
         (async()=>{
             
-            // click buy now button
-            let buynowBtn='#container > div > div.t-0M7P._3GgMx1._2doH3V > div._3e7xtJ > div._1HmYoV.hCUpcT > div._1HmYoV._35HD7C.col-5-12._3KsTU0 > div:nth-child(2) > div > ul > li:nth-child(2) > form > button';
-            await page.waitForSelector(buynowBtn);
-            const isDisabled = await page.$eval(buynowBtn, (button) => {
-                return button.disabled;
-            });
-            // check if buttton is disabled
-            if(isDisabled){
-                console.log('button is disabled');
-                await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
-                order(page);
-                return;
-            }
-            await page.click(buynowBtn);
-            
-            // delivery address
-            // ########### Uncomment below code if you have multiple addressess linked to your account ############################
-            // let addressBtn='#CNTCTA4CAB47D08304FE9BA8651CED > button'
-            // await page.waitForSelector(addressBtn);
-            // await page.click(addressBtn);
-        
-            // click continue with quantity button
-            let qtyBtn='#to-payment > button';
-            await page.waitForSelector(qtyBtn);
-            await page.click(qtyBtn);
-        
-            //Choose Phonepe radio button
-            let phonepeRadioBtn='#PHONEPE'
-            await page.waitForSelector(phonepeRadioBtn);
-            await page.evaluate((phonepeRadioBtn) => document.querySelector(phonepeRadioBtn).click(), phonepeRadioBtn);
-            
-            //click Continue with phonepe button
-            let contiueToPayBtn='div._1Ua1Gl > div > div._3B4tat > div._1GRhLX._38vNoF > div > div > div:nth-child(2) > div > label._8J-bZE._3C6tOa._1syowc._2i24Q8._1Icwrf > div._2o59RR._27CukN > div > div > div._3MGkT3 > button'
-            await page.waitForSelector(contiueToPayBtn);
-            await page.evaluate((contiueToPayBtn) => document.querySelector(contiueToPayBtn).click(), contiueToPayBtn);
-                     
-        })()
+            try{
+                // click buy now button
+                let buynowBtn='#container > div > div.t-0M7P._3GgMx1._2doH3V > div._3e7xtJ > div._1HmYoV.hCUpcT > div._1HmYoV._35HD7C.col-5-12._3KsTU0 > div:nth-child(2) > div > ul > li:nth-child(2) > form > button';
+                await page.waitForSelector(buynowBtn, { timeout: 10000 });
+                const isDisabled = await page.$eval(buynowBtn, (button) => {
+                    return button.disabled;
+                });
 
+                // check if buttton is disabled
+                if(isDisabled){
+                    console.log('button is disabled');
+                    await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
+                    order(page);
+                    return;
+                }
+                await page.click(buynowBtn);
+                
+                // delivery address
+                // ########### Uncomment below code if you have multiple addressess linked to your account ############################
+                // let addressBtn='#CNTCTA4CAB47D08304FE9BA8651CED > button'
+                // await page.waitForSelector(addressBtn);
+                // await page.click(addressBtn);
+            
+                // click continue with quantity button
+                let qtyBtn='#to-payment > button';
+                await page.waitForSelector(qtyBtn);
+                await page.click(qtyBtn);
+            
+                //Choose Phonepe radio button
+                let phonepeRadioBtn='#PHONEPE'
+                await page.waitForSelector(phonepeRadioBtn);
+                await page.evaluate((phonepeRadioBtn) => document.querySelector(phonepeRadioBtn).click(), phonepeRadioBtn);
+                
+                //click Continue with phonepe button
+                let contiueToPayBtn='div._1Ua1Gl > div > div._3B4tat > div._1GRhLX._38vNoF > div > div > div:nth-child(2) > div > label._8J-bZE._3C6tOa._1syowc._2i24Q8._1Icwrf > div._2o59RR._27CukN > div > div > div._3MGkT3 > button'
+                await page.waitForSelector(contiueToPayBtn);
+                await page.evaluate((contiueToPayBtn) => document.querySelector(contiueToPayBtn).click(), contiueToPayBtn);
+            }
+            catch(err){
+                // reload page if any error occurs
+                await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
+                checkForSaleStart(page);
+            }
+                      
+        })()
     } catch (err) {
         console.error(err)
     }
